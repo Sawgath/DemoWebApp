9f12d9dc6e9b4c5c8f0ec22832188cda5bc10ffd
Sawgath Jahan <sawjahan@gmail.com>
2017-08-14 18:13:11 +1000

    change 3

--- src/main/java/com/Jahan/DemoWebApp/controller/InputMovieController.java
+++ src/main/java/com/Jahan/DemoWebApp/controller/InputMovieController.java
@@ -2,6 +2,7 @@ package com.Jahan.DemoWebApp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,5 +18,14 @@ public class InputMovieController {
 		return "MoiveInput";
 		
 	}
+	@RequestMapping(value="/Input", method=RequestMethod.POST)
+    public String checkPersonInfo(BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "";
+        }
+
+        return "";
+    }
 
 }
